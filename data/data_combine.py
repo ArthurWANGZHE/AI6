acd3d771fca103b7f8ef0acacd3b9a9f6131162b
ArthurWANGZHE <3098016342@qq.com>
2023-08-08 23:44:40 +0800

    修正测试

--- data/data_combine.py
+++ data/data_combine.py
@@ -2,7 +2,26 @@
 for i in range(1,39):
     with open(f'book{i}.txt', 'r', encoding='utf-8') as file:
         text = file.read()
-    with open('book.txt', 'a', encoding='utf-8') as file:
+# 去除空格和标点符号
+        text = text.replace(' ', '')
+        text = text.replace('\n', '')
+        text = text.replace('\r', '')
+        text = text.replace('，', '')
+        text = text.replace('。', '')
+        text = text.replace('？', '')
+        text = text.replace('！', '')
+        text = text.replace('；', '')
+        text = text.replace('：', '')
+        text = text.replace('“', '')
+        text = text.replace('”', '')
+        text = text.replace('.', '')
+        text = text.replace('…', '')
+        text = text.replace('《', '')
+        text = text.replace('》', '')
+        text = text.replace('【', '')
+        text = text.replace('】', '')
+
+    with open('../RUN/book0.txt', 'a', encoding='utf-8') as file:
         file.write(text)
     print(i)
 
