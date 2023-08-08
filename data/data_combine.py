b2dd5fc3a9ad13b6f4e77f3910edeedebef1c29b
ArthurWANGZHE <3098016342@qq.com>
2023-08-08 13:55:03 +0800

    修正测试

--- data/data_combine.py
+++ data/data_combine.py
@@ -1,5 +1,5 @@
 # This file is used to combine all the books into one file
-for i in range(1,10):
+for i in range(1,39):
     with open(f'book{i}.txt', 'r', encoding='utf-8') as file:
         text = file.read()
     with open('book.txt', 'a', encoding='utf-8') as file:
