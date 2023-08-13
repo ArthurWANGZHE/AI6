7031b91fd2e9795b68b399f3b8e0c7e8e566b341
ArthurWANGZHE <3098016342@qq.com>
2023-08-13 12:42:40 +0800

    bert_to_bert

--- bert_to_bert.py
+++ bert_to_bert.py
@@ -1,46 +1,106 @@
 import torch
 import torch.nn as nn
 from transformers import BertModel, BertTokenizer
+from torch.utils.data import DataLoader, Dataset
+from torch.nn.utils.rnn import pad_sequence
 
-BERT_PATH = '../bert-base-chinese'
+BERT_PATH = 'bert-base-chinese'
+
+
+class MyDataset(Dataset):
+    def __init__(self, data):
+        self.data = data
+
+    def __len__(self):
+        return len(self.data)
+
+    def __getitem__(self, index):
+        return self.data[index]
+
+
 class BertToBertModel(nn.Module):
     def __init__(self):
         super(BertToBertModel, self).__init__()
-
-        # 初始化编码器和解码器的Bert模型和分词器
         self.encoder_bert = BertModel.from_pretrained(BERT_PATH)
         self.decoder_bert = BertModel.from_pretrained(BERT_PATH)
-
-        # 其他需要的层和参数
         self.linear = nn.Linear(self.encoder_bert.config.hidden_size, self.decoder_bert.config.hidden_size)
 
     def forward(self, input_ids, attention_mask):
-        # 编码输入文本
         encoder_outputs = self.encoder_bert(input_ids=input_ids, attention_mask=attention_mask)
         encoded_hidden_states = encoder_outputs.last_hidden_state
-
-        # 使用线性层将编码后的表示转换为解码器的表示
         decoder_input = self.linear(encoded_hidden_states)
 
-        # 解码
+        # 使用 decoder_input 作为输入
         decoder_outputs = self.decoder_bert(input_ids=decoder_input, attention_mask=attention_mask)
         decoded_hidden_states = decoder_outputs.last_hidden_state
-
         return decoded_hidden_states
 
-# 初始化模型
-model = BertToBertModel()
 
-# 输入示例（需要进行中文分词）
+# 示例训练数据
+train_data = ["这是一段中文小说。", "另一段小说内容。"]
+
+# 数据预处理
 tokenizer = BertTokenizer.from_pretrained(BERT_PATH)
-text = "这是一段中文小说。"
-input_ids = tokenizer.encode(text, add_special_tokens=True)
-attention_mask = [1] * len(input_ids)
-input_ids = torch.tensor(input_ids).unsqueeze(0)  # 添加batch维度
-attention_mask = torch.tensor(attention_mask).unsqueeze(0)  # 添加batch维度
+input_ids = [tokenizer.encode(text, add_special_tokens=True) for text in train_data]
+attention_masks = [[1] * len(ids) for ids in input_ids]
 
-# 前向传播
-output = model(input_ids, attention_mask)
+# 创建数据加载器
+batch_size = 2
+train_dataset = MyDataset(list(zip(input_ids, attention_masks)))
+train_loader = DataLoader(train_dataset, batch_size=batch_size, shuffle=True)
 
-# 输出解码后的表示
-print(output)
+# 初始化模型并将其移至GPU
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+model = BertToBertModel().to(device)
+
+# 训练循环
+num_epochs = 5
+optimizer = torch.optim.AdamW(model.parameters(), lr=2e-5)
+criterion = nn.CrossEntropyLoss()
+
+for epoch in range(num_epochs):
+    model.train()
+    total_loss = 0
+
+    for batch_input_ids, batch_attention_masks in train_loader:
+        batch_input_ids = batch_input_ids.to(device)
+        batch_attention_masks = batch_attention_masks.to(device)
+
+        optimizer.zero_grad()
+
+        outputs = model(input_ids=batch_input_ids, attention_mask=batch_attention_masks)
+        loss = criterion(outputs.view(-1, outputs.size(-1)), batch_input_ids.view(-1))
+
+        loss.backward()
+        optimizer.step()
+
+        total_loss += loss.item()
+
+    average_loss = total_loss / len(train_loader)
+    print(f"Epoch [{epoch + 1}/{num_epochs}], Average Loss: {average_loss:.4f}")
+
+print("Training finished.")
+def generate_continuation(prompt_text, max_length=50):
+    model.eval()
+
+    # 将输入文本编码为input_ids
+    input_ids = tokenizer.encode(prompt_text, add_special_tokens=True)
+    input_ids = torch.tensor(input_ids).unsqueeze(0).to(device)
+
+    # 生成续写文本
+    with torch.no_grad():
+        for _ in range(max_length):
+            attention_mask = torch.ones(input_ids.shape, device=device)
+            outputs = model(input_ids=input_ids, attention_mask=attention_mask)
+            predicted_ids = outputs.argmax(dim=-1)[:, -1]
+            input_ids = torch.cat((input_ids, predicted_ids.unsqueeze(-1)), dim=-1)
+
+    # 解码生成的文本
+    generated_text = tokenizer.decode(input_ids[0].tolist(), skip_special_tokens=True)
+    return generated_text
+
+
+# 输入一个续写的起始句子，生成续写内容
+prompt = "故事的开始"
+generated_text = generate_continuation(prompt, max_length=100)
+print(generated_text)
